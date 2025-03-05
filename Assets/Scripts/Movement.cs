9c57d7005d512029e57dddfebb51d0301e949db3
Chloe <duyenpham.june.1995@gmail.com>
2025-03-05 14:47:20 +1100

    using Rotation with InputAction

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+     [SerializeField] InputAction rotation;
     [SerializeField] float thrustStrength = 1000f;
+    [SerializeField] float rotationStrength = 1000f;
     Rigidbody rb;
     private void Start() {
         rb = GetComponent<Rigidbody>();
@@ -12,13 +14,40 @@ public class Movement : MonoBehaviour
 
     private void OnEnable() {
         thrust.Enable();
+        rotation.Enable();
     }
 
-    private void FixedUpdate() {
-        if (thrust.IsPressed()) {
-            Debug.Log("Pressed");
+    private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
+        {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
     }
 
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput < 0) // left arrow: negative values
+        {
+            ApplyRotation(rotationStrength);
+
+        }
+        else if (rotationInput > 0) // right arrow: positive values
+        {             
+            ApplyRotation(-rotationStrength);
+        }
+
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+    }
 }
