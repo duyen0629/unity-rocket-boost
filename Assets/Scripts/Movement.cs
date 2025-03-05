533fdca50999c3627ccdc13ea2c62ac61e8c574e
Chloe <duyenpham.june.1995@gmail.com>
2025-03-05 13:51:13 +1100

    using AddRelativeForce and Vector3 to make the rocket fly

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,14 +4,20 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] float thrustStrength = 1000f;
+    Rigidbody rb;
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnEnable() {
         thrust.Enable();
     }
 
-    private void Update() {
+    private void FixedUpdate() {
         if (thrust.IsPressed()) {
-            Debug.Log("Thrust is pressed");
+            Debug.Log("Pressed");
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
     }
 
